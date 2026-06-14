2dceeffc76001790ab071233ff6d2f5b209c360b
Miguel Angel Cabello Mesa <miguel844@gmail.com>
2026-06-14 13:53:57 +0200

    Funcionalidad al boton +/- y arreglos varios

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -1,6 +1,6 @@
 export default class Calculadora {
     constructor() {
-        this.valorActual = "";
+        this.valorActual = "0";
         this.operador = "";
         this.primerNumero = 0;
         this.segundoNumero = 0;
@@ -47,13 +47,31 @@ export default class Calculadora {
         }
         this.valorActual = "" + this.resultado;
     }
+    ponerDecimal() {
+        if (this.valorActual.includes(".")) {
+            console.log("Solo puede haber un punto");
+        } else {
+            this.valorActual += ".";
+        }
+    }
+    
+    cambiarPositivo() {
+
+        if (this.valorActual !== "0" && this.valorActual !== "0.") {
+            if (this.valorActual[0] == "-") {
+                this.valorActual = this.valorActual.slice(1);
+            } else {
+                this.valorActual = "-" + this.valorActual;
+            }
+        }
+    }
 
     limpiar() {
-        this.valorActual = "";
+        this.valorActual = "0";
     }
 
     reset() {
-        this.valorActual = "";
+        this.valorActual = "0";
         this.operador = "";
         this.primerNumero = 0;
         this.segundoNumero = 0;
@@ -61,7 +79,7 @@ export default class Calculadora {
     }
 
     borrar() {
-        if (this.valorActual.length > 1) {
+        if (this.valorActual === "-" || this.valorActual.length > 1) {
             this.valorActual = this.valorActual.slice(0, -1);
         } else if (this.valorActual == "0") {
         } else {
--- js/main.js
+++ js/main.js
@@ -14,14 +14,7 @@ botones.forEach(boton => {
             resultado.textContent = calc.valorActual;
 
         } else if (entrada == ".") {
-            if (calc.valorActual == 0 || calc.valorActual == "") {
-                calc.valorActual += "0.";
-            } else if (calc.valorActual.includes(".")) {
-                console.log("Solo puede haber un punto");
-            } else {
-                calc.valorActual += ".";
-            }
-
+            calc.ponerDecimal();
             resultado.textContent = calc.valorActual;
 
         } else if (entrada == "=") {
@@ -30,21 +23,23 @@ botones.forEach(boton => {
 
         } else if (entrada == "C") {
             calc.limpiar();
-            resultado.textContent = 0;
+            resultado.textContent = calc.valorActual;
 
         } else if (entrada == "CE") {
             calc.reset();
-            resultado.textContent = 0;
+            resultado.textContent = calc.valorActual;
 
         } else if (entrada == "←") {
             calc.borrar();
+            resultado.textContent = calc.valorActual;
+        
+        } else if (entrada == "+/-") {
+            calc.cambiarPositivo();
             resultado.textContent = calc.valorActual;
 
         } else {
             calc.agregarOperador(entrada);
         }
-
-        console.log(calc.valorActual);
 
     });
 });
